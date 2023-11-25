34c92b57e75596cef994678bdd225303cf39b7cb
tobiasgiacri <tobiasgiacri@gmail.com>
2023-11-25 16:57:48 -0300

    crear-esquema.sql funcionando , se borraron PKs, y timestamps

--- scripts/esquema/crear-esquema.sql
+++ scripts/esquema/crear-esquema.sql
@@ -1,88 +1,123 @@
 -- SI SE VAN A AGREGAR MÁS TABLAS ACÁ, POR FAVOR HACERLO EN EL ORDEN DEL ARCHIVO DEL MLR
 
-CREATE TABLE IF NOT EXISTS nivel_seguridad(
-    nivel SERIAL PRIMARY KEY,
-    descripcion VARCHAR(100) NOT NULL
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'nivel_seguridad')
+BEGIN
+    CREATE TABLE nivel_seguridad (
+        nivel INT PRIMARY KEY,
+        descripcion VARCHAR(100) NOT NULL
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS area(
-    numero_area SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    nivel INT NOT NULL,
-    FOREIGN KEY (nivel) REFERENCES nivel_seguridad(nivel)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'area')
+BEGIN
+    CREATE TABLE area (
+        numero_area INT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        nivel INT NOT NULL,
+        FOREIGN KEY (nivel) REFERENCES nivel_seguridad(nivel)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS evento(
-    id_evento SERIAL PRIMARY KEY,
-    descripcion VARCHAR(100) NOT NULL,
-    fecha_hora TIMESTAMP NOT NULL,
-    id_area INT NOT NULL,
-    FOREIGN KEY (id_area) REFERENCES area(id_area)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'evento')
+BEGIN
+    CREATE TABLE evento (
+        id_evento INT PRIMARY KEY,
+        descripcion VARCHAR(100) NOT NULL,
+        fecha_hora DATETIME NOT NULL,
+        id_area INT NOT NULL,
+        FOREIGN KEY (id_area) REFERENCES area(numero_area)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal(
-    id_personal SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    dni VARCHAR(10) NOT NULL,
-    genero VARCHAR(1) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    contraseña VARCHAR(100) NOT NULL,
-    huella TEXT NOT NULL,    
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal')
+BEGIN
+    CREATE TABLE personal (
+        id_personal INT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        apellido VARCHAR(100) NOT NULL,
+        dni VARCHAR(10) NOT NULL,
+        genero CHAR(1) NOT NULL,
+        fecha_nacimiento DATE NOT NULL,
+        contraseña VARCHAR(100) NOT NULL,
+        huella TEXT NOT NULL    
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal_jerarquico(
-    id_personal INT PRIMARY KEY,
-    numero_area INT PRIMARY KEY,
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-    FOREIGN KEY (numero_area) REFERENCES area(numero_area)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal_jerarquico')
+BEGIN
+    CREATE TABLE personal_jerarquico (
+        id_personal INT PRIMARY KEY,
+        numero_area INT ,  
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal),
+        FOREIGN KEY (numero_area) REFERENCES area(numero_area)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS especialidad(
-    id_especialidad SERIAL PRIMARY KEY,
-    descripcion VARCHAR(100) NOT NULL
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'especialidad')
+BEGIN
+    CREATE TABLE especialidad (
+        id_especialidad INT PRIMARY KEY,
+        descripcion VARCHAR(100) NOT NULL
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal_profesional(
-    id_personal INT PRIMARY KEY,
-    id_especialidad INT PRIMARY KEY,
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-    FOREIGN KEY (id_especialidad) REFERENCES especialidad(id_especialidad)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal_profesional')
+BEGIN
+    CREATE TABLE personal_profesional (
+        id_personal INT PRIMARY KEY,
+        id_especialidad INT,
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal),
+        FOREIGN KEY (id_especialidad) REFERENCES especialidad(id_especialidad)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal_no_profesional(
-    id_personal INT PRIMARY KEY,
-    id_nivel_seguridad INT PRIMARY KEY,
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-    FOREIGN KEY (id_nivel_seguridad) REFERENCES nivel_seguridad(nivel)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal_no_profesional')
+BEGIN
+    CREATE TABLE personal_no_profesional (
+        id_personal INT PRIMARY KEY,
+        id_nivel_seguridad INT,
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal),
+        FOREIGN KEY (id_nivel_seguridad) REFERENCES nivel_seguridad(nivel)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal_profesional_permanente(
-    id_personal INT PRIMARY KEY,
-    numero_area INT PRIMARY KEY,
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-    FOREIGN KEY (numero_area) REFERENCES area(numero_area)
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal_profesional_permanente')
+BEGIN
+    CREATE TABLE personal_profesional_permanente (
+        id_personal INT PRIMARY KEY,
+        numero_area INT,
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal),
+        FOREIGN KEY (numero_area) REFERENCES area(numero_area)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS personal_profesional_contratado(
-    id_personal INT PRIMARY KEY,
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-)
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'personal_profesional_contratado')
+BEGIN
+    CREATE TABLE personal_profesional_contratado (
+        id_personal INT PRIMARY KEY,
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS contrato_de_trabajo(
-    id_contrato INT PRIMARY KEY,
-    tarea TEXT NOT NULL,
-    periodo_inicio DATE NOT NULL,
-    periodo_fin DATE NOT NULL
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'contrato_de_trabajo')
+BEGIN
+    CREATE TABLE contrato_de_trabajo (
+        id_contrato INT PRIMARY KEY,
+        tarea TEXT NOT NULL,
+        periodo_inicio DATE NOT NULL,
+        periodo_fin DATE NOT NULL
+    );
+END;
 
-CREATE TABLE IF NOT EXISTS auditoria(
-    id_auditoria SERIAL PRIMARY KEY,
-    fecha_hora TIMESTAMP NOT NULL,
-    resultado VARCHAR(100) NOT NULL,
-    id_contrato INT PRIMARY KEY,
-    id_personal INT PRIMARY KEY,
-    FOREIGN KEY (id_contrato) REFERENCES contrato_de_trabajo(id_contrato),
-    FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
-);
-
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'auditoria')
+BEGIN
+    CREATE TABLE auditoria (
+        id_auditoria INT PRIMARY KEY,
+        fecha_hora DATETIME NOT NULL,
+        resultado VARCHAR(100) NOT NULL,
+        id_contrato INT NOT NULL,
+        id_personal INT NOT NULL,
+        FOREIGN KEY (id_contrato) REFERENCES contrato_de_trabajo(id_contrato),
+        FOREIGN KEY (id_personal) REFERENCES personal(id_personal)
+    );
+END;
