6316741f381afbf9b720486b55ba41cda412dfed
nahuel-42 <nahuelisaias.nievas.contractor@gdevtools.com>
2023-11-26 14:09:53 -0300

    Fix de update para no arruinar los datos de MOCK

--- appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/TransaccionPrueba.java
+++ appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/TransaccionPrueba.java
@@ -15,14 +15,16 @@ public class TransaccionPrueba extends Query{
             conexion.setAutoCommit(false);
 
             stmt = conexion.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM area");
 
+            /*Esta comentado para no arruinar los datos mock
             stmt.executeUpdate("INSERT INTO area (numero_area, nombre, nivel) VALUES\r\n" + //
             		"(6, 'Juan Pérez', 1),\r\n" + //
             		"(7, 'María Rodríguez', 2),\r\n" + //
             		"(8, 'Luis González', 3),\r\n" + //
             		"(9, 'Ana Martínez', 4),\r\n" + //
             		"(10, 'Pedro Sánchez', 5);");
-
+*/
             //Si salio bien
             conexion.commit();
             System.out.println("Transaccion completada exitosamente");
