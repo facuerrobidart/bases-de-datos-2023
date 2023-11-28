45c6a1e097f18cb551d45f16e161a4799a057a60
nahuel-42 <nahuelisaias.nievas.contractor@gdevtools.com>
2023-11-28 07:58:32 -0300

    Transaccion finalizada

--- appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/TRX_CREAR_PERSONAL_JERARQUICO.java
+++ appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/TRX_CREAR_PERSONAL_JERARQUICO.java
@@ -16,13 +16,20 @@ public class TRX_CREAR_PERSONAL_JERARQUICO extends Query {
 
             stmt = conexion.createStatement();
             
+            System.out.println("Transaccion a ejecutar: "+ "INSERT INTO personal (id_personal, nombre, apellido, dni, genero, fecha_nacimiento, contraseña, huella) VALUES "
+            		+ "("+ registro.getIdPersonal() +", '"+registro.getNombre()+"', '"+registro.getApellido()+"', "+registro.getDni()+", '"+registro.getGenero()+"', '"+registro.getFechaNacimiento()+"', '"+registro.getContraseña()+"', '"+registro.getHuella()+"')");
+            
             stmt.executeUpdate("INSERT INTO personal (id_personal, nombre, apellido, dni, genero, fecha_nacimiento, contraseña, huella) VALUES "
-            		+ "("+ registro.getIdPersonal() +", "+registro.getNombre()+", "+registro.getApellido()+", "+registro.getDni()+", "+registro.getGenero()+", "+registro.getFechaNacimiento()+", "+registro.getContraseña()+", "+registro.getHuella()+"),");
-
+            		+ "("+ registro.getIdPersonal() +", '"+registro.getNombre()+"', '"+registro.getApellido()+"', "+registro.getDni()+", '"+registro.getGenero()+"', '"+registro.getFechaNacimiento()+"', '"+registro.getContraseña()+"', '"+registro.getHuella()+"')");
+            
+            System.out.println("INSERT INTO personal_jerarquico (id_personal, numero_area, fecha_de_inicio)"
+            		+ "VALUES "
+            		+ "("+ registro.getIdPersonal() +", '"+registro.getNumero_area()+"', '"+registro.getFecha_de_inicio()+"')");
+            
             stmt.executeUpdate("INSERT INTO personal_jerarquico (id_personal, numero_area, fecha_de_inicio)"
             		+ "VALUES "
-            		+ "("+ registro.getIdPersonal() +", "+registro.getNumero_area()+", "+registro.getFecha_de_inicio()+"),");
-            
+            		+ "("+ registro.getIdPersonal() +", '"+registro.getNumero_area()+"', '"+registro.getFecha_de_inicio()+"',3)");
+            //	    + "("+ registro.getIdPersonal() +", '"+registro.getNumero_area()+"', '"+registro.getFecha_de_inicio()+"')");
             conexion.commit(); 
             System.out.println("Transaccion completada exitosamente");
         } catch (SQLException e) {
--- appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/Utils.java
+++ appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/Utils.java
@@ -34,7 +34,19 @@ public class Utils {
         String contraseña = sc.nextLine();
         System.out.println("Ingrese la huella del personal");
         String huella = sc.nextLine();
-        Registro registro = new Registro( nombre, apellido, dni, genero, fechaNacimientoSql, contraseña, huella);
+        System.out.println("Ingrese el numero de area del personal");
+        int numero_area = sc.nextInt();
+        sc.nextLine();  // consume the newline
+        System.out.println("Ingrese la fecha de inicio del personal");
+        String fecha_de_inicio = sc.nextLine();
+        java.util.Date fecha_de_inicioDate=null;
+        try {
+            fecha_de_inicioDate = sdf.parse(fecha_de_inicio);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }       
+        Date fecha_de_inicioSql = new Date(fecha_de_inicioDate.getTime());
+        Registro registro = new Registro( nombre, apellido, dni, genero, fechaNacimientoSql, contraseña, huella, numero_area, fecha_de_inicioSql );
         return registro;
     }
 
