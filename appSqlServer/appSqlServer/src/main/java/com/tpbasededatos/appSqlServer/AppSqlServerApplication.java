202fec2e3aa02d942f7be8278349fd6d0732b19f
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2023-11-29 21:19:56 -0300

    fix app

--- appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/AppSqlServerApplication.java
+++ appSqlServer/appSqlServer/src/main/java/com/tpbasededatos/appSqlServer/AppSqlServerApplication.java
@@ -1,9 +1,6 @@
 package com.tpbasededatos.appSqlServer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class AppSqlServerApplication {
@@ -22,12 +19,18 @@ public class AppSqlServerApplication {
 				System.out.println("1. Crear Personal Jerarquico");
 				System.out.println("2. Crear Personal Profesional");
 				System.out.println("3. Crear Personal No profesional");
+				System.out.println("4. Ver Areas");
+
 				opcion = sc.nextInt();
 				switch (opcion) {
 				case 1:
 					Registro registro= Utils.InputRegistrarPersonalJerarquico();
 					TRX_CREAR_PERSONAL_JERARQUICO trx = new TRX_CREAR_PERSONAL_JERARQUICO(conexion);
 		    		trx.execute(registro);
+					break;
+				case 4:
+					QueryPruebaEjemplo query = new QueryPruebaEjemplo(conexion);
+					query.execute();
 					break;
 				default:
 					break;
